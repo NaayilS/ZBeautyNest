c15029fa29aaa48fc2057db6586fb8c126086fdd
NaayilS <sadiqnaayil@gmail.com>
2024-10-07 03:33:48 -0500

    Updated product schema and added reviews array to Product model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,34 +1,14 @@
-import mongoose from 'mongoose';
-
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    ingredients: {
-        type: [String],
-        required: true,
-    },
-    reviews: {
-        type: [String],
-        required: false,
-    },
-}, {
-    timestamps: true,
-});
-
-const Product = mongoose.model('Product', productSchema);
-export default Product;
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    rating: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true },
+    ingredients: { type: [String], required: true },
+    reviews: [
+        {
+            user: { type: String, required: true },
+            rating: { type: Number, required: true },
+            comment: { type: String, required: true },
+        },
+    ],
+}, { timestamps: true });
